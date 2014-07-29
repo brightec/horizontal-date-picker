5d88a8c48386a1c6db06e93dba3763aa0c719a6d
Cameron Cooke <cameron@brightec.co.uk>
2014-07-29 12:15:16 +0100

    Initial Commit

---
+++ HorizontalDatePicker/LCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCAppDelegate.h
+//  HorizontalDatePicker
+//
+//  Created by Cameron Cooke on 29/07/2014.
+//  Copyright (c) 2014 Brightec Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HorizontalDatePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HorizontalDatePicker
+//
+//  Created by Cameron Cooke on 29/07/2014.
+//  Copyright (c) 2014 Brightec Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+    }
+}
